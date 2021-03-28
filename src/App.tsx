dbaa840f68bcf967346aedeb07ef8885333f8f40
Rubens Fidelis <r2fidelis@r2fidelis.com>
2021-03-28 01:09:21 -0300

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { GlobalStyle } from './styles/global'
+import { Header } from './components/Header'
 
 export function App() {
   return (
     <div className='App'>
+      <Header />
       <h1>dtmoney</h1>
       <GlobalStyle />
     </div>
